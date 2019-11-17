192fe97058a4b083a4e20b4459d2f2f6ebc631aa
palangana-estelar <35443602+palangana-estelar@users.noreply.github.com>
2019-11-16 18:39:19 -0800

    Update lookup_mac.sh
    bash echo needs -e to interpret "\n"

--- lookup_mac.sh
+++ lookup_mac.sh
@@ -4,17 +4,17 @@
 # Give a list of 
 # active connections, and then WHOIS to see who you are connected to
 ######
-echo " \nBelow is a list of ips with active TCP connections to you \n "
+echo -e ' \nBelow is a list of ips with active TCP connections to you \n'
 #netstat -tuna -p tcp | awk '{ print $5 }' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq
 
 # netstat (macos syntax) to get all open tcp connection
 # then grep only remote addresses
 # used grep because awk includes table info from netsta result
 # then sort and uniq to remove duplicate ips
-echo "\nYou are connected to: \n "
+echo -e '\nYou are connected to: \n'
 for i in $(netstat -tuna -p tcp | awk '{ print $5 }' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq); do
 	#echo "\n"
 	echo $i
 	whois $i | grep -e OrgName -e Country -e CIDR
-	echo "----------------- \n"
+	echo -e '----------------- '
 done
